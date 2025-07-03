2533b0b679c73e044a5f41167534c19834949728
entityJY <jomathya07@gmail.com>
2025-07-03 13:31:22 -0700

    Basic moving and flipping of pieces

--- rust/src/game_piece.rs
+++ rust/src/game_piece.rs
@@ -1,4 +1,6 @@
 use godot::classes::InputEvent;
+use godot::classes::InputEventMouseMotion;
+use godot::obj::WithBaseField;
 use godot::prelude::*;
 use godot::classes::Node2D;
 use godot::classes::INode2D;
@@ -8,23 +10,46 @@ use godot::classes::Sprite2D;
 #[derive(GodotClass)]
 #[class(base=Node2D)]
 struct Piece {
+
+    #[export]
     facing_white: bool,
+    following_mouse: bool,
+
     #[export]
     white_sprite: Option<Gd<Sprite2D>>,
     #[export]
     black_sprite: Option<Gd<Sprite2D>>,
+
+    previous_position: Vector2,
 
     base: Base<Node2D>
 }
 
 #[godot_api]
 impl INode2D for Piece {
+
     fn init(base: Base<Node2D>) -> Self {
         Self {
             facing_white: true,
+            following_mouse: false,
             white_sprite: None,
             black_sprite: None,
+            previous_position: Vector2::ZERO,
             base
+        }
+    }
+
+    fn ready(&mut self) {
+        self.previous_position = self.base().get_position();
+        self.enable_white();
+        if !self.facing_white {self.enable_black();};
+    }
+
+    fn input(&mut self, event: Gd<InputEvent>) {
+        if self.following_mouse {
+            if let Ok(e) = event.clone().try_cast::<InputEventMouseMotion>() {
+                self.base_mut().set_position(e.get_position());
+            }
         }
     }
 }
@@ -33,8 +58,17 @@ impl INode2D for Piece {
 pub impl Piece {
 
     #[func]
-    pub fn test(&mut self, _: Gd<Node>, event: Gd<InputEvent>, _: i32) {
+    pub fn click(&mut self, _: Gd<Node>, event: Gd<InputEvent>, _: i32) {
         if event.is_action_pressed("LeftClick") {
+            self.following_mouse = !self.following_mouse;
+            if self.following_mouse {
+                self.previous_position = self.base().get_position();
+            }
+        } else if event.is_action_pressed("RightClick") && self.following_mouse {
+            self.following_mouse = false;
+            let pos = self.previous_position;
+            self.base_mut().set_position(pos);
+        } else if event.is_action_pressed("RightClick") && !self.following_mouse {
             self.flip_piece();
         }
     }
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,6 +3,7 @@ use godot::prelude::*;
 struct MyExtension;
 
 // don't forget to do: mod {file_name};
+mod utils;
 mod game_piece;
 
 #[gdextension]
